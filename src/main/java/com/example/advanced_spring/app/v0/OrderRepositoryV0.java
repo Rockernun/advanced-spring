8f86e1fd74d5b9ce294b898f4692edf7fbb2de68
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-11 13:30:01 +0900

    feat: Order에 대한 Repository, Service, Controller 클래스 추가
    - 단순 itemId를 받아 주문 정보를 저장하는 로직 추가
    - 성공 시 "success" 반환
    - itemId가 "ex" 일 경우, IllegalStateException 예외 발생
    - 주문 정보 저장하는데 1초

---
+++ src/main/java/com/example/advanced_spring/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,26 @@
+package com.example.advanced_spring.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+
+        // 주문 정보를 저장하는데 1초 걸린다고 가정
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
