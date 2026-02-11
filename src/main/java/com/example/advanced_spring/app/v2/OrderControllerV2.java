4d530ce9af044b8998b54fc6b4faaab30bfbe3c5
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-11 15:21:39 +0900

    feat: 주문 로직에 로그 생성기 V2 적용

---
+++ src/main/java/com/example/advanced_spring/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package com.example.advanced_spring.app.v2;
+
+import com.example.advanced_spring.trace.TraceStatus;
+import com.example.advanced_spring.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "success";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
