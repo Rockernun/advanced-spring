e651bad6519a445ad6bc106cbdc67ca0ebfed0ab
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-12 15:43:00 +0900

    feat(ThreadLocalLogTrace): ThreadLocal을 적용한 로그 생성기 추가

---
+++ src/main/java/com/example/advanced_spring/trace/logtrace/ThreadLocalLogTrace.java
@@ -0,0 +1,81 @@
+package com.example.advanced_spring.trace.logtrace;
+
+import com.example.advanced_spring.trace.TraceId;
+import com.example.advanced_spring.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocalLogTrace implements LogTrace {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    private final ThreadLocal<TraceId> traceIdHolder = new ThreadLocal<>();
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = traceIdHolder.get();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(),
+                addSpace(START_PREFIX, traceId.getDepth()), message);
+
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    private void syncTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId == null) {
+            traceIdHolder.set(new TraceId());
+        } else {
+            traceIdHolder.set(traceId.createNextId());
+        }
+    }
+
+    @Override
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        long resultTimeMs = System.currentTimeMillis() - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(),
+                    addSpace(COMPLETE_PREFIX, traceId.getDepth()), status.getMessage(),
+                    resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
+                    addSpace(EX_PREFIX, traceId.getDepth()), status.getMessage(), resultTimeMs,
+                    e.toString());
+        }
+
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+        TraceId traceId = traceIdHolder.get();
+        if (traceId.isFirstDepth()) {
+            traceIdHolder.remove();
+        } else {
+            traceIdHolder.set(traceId.createNextId());
+        }
+    }
+
+    private String addSpace(String prefix, int depth) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < depth; i++) {
+            sb.append( (i == depth - 1) ? "|" + prefix : "| ");
+        }
+
+        return sb.toString();
+    }
+}
