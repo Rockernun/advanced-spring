a5def468e079d746bbdb35936bcbccab670284e5
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-13 13:39:17 +0900

    test(TemplateMethodTest): 템플릿 메서드 패턴 도입을 위한 간단한 로직 수행 테스트 추가

---
+++ src/test/java/com/example/advanced_spring/trace/template/TemplateMethodTest.java
@@ -0,0 +1,34 @@
+package com.example.advanced_spring.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethod() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+
+        log.info("비즈니스 로직1 실행");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = System.currentTimeMillis();
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+
+        log.info("비즈니스 로직2 실행");
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = System.currentTimeMillis();
+        log.info("resultTime={}", resultTime);
+    }
+}
