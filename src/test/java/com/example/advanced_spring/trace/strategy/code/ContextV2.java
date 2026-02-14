7cac01205f0d30ae2d5506e9263e35af39553fa2
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-14 20:13:44 +0900

    feat(ContextV2): 전략을 필드가 아닌 파라미터로 전달 받는 ContextV2 클래스 추가

---
+++ src/test/java/com/example/advanced_spring/trace/strategy/code/ContextV2.java
@@ -0,0 +1,22 @@
+package com.example.advanced_spring.trace.strategy.code;
+
+import com.example.advanced_spring.trace.strategy.code.strategy.Strategy;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 전략을 파라미터로 전달 받는 방식
+ */
+@Slf4j
+public class ContextV2 {
+
+    public void execute(Strategy strategy) {
+        long startTime = System.currentTimeMillis();
+
+        // 비즈니스 로직 실행 (위임)
+        strategy.call();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime: {}", resultTime);
+    }
+}
