e99060aa11ced7a4fbfaf01df92407144cd3ee9f
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-14 19:40:39 +0900

    feat(ContextV1): 변하지 않는 부분을 보관하기 위해 ContextV1 클래스 추가
    - 변하지 않는 로그 로직과 향후 위임받을 비즈니스 로직만 존재

---
+++ src/test/java/com/example/advanced_spring/trace/strategy/code/ContextV1.java
@@ -0,0 +1,28 @@
+package com.example.advanced_spring.trace.strategy.code;
+
+import com.example.advanced_spring.trace.strategy.code.strategy.Strategy;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 필드에 전략을 보관하는 방식
+ */
+@Slf4j
+public class ContextV1 {
+
+    private final Strategy strategy;
+
+    public ContextV1(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+
+        // 비즈니스 로직 실행 (위임)
+        strategy.call();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime: {}", resultTime);
+    }
+}
