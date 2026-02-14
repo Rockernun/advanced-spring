40f8ead7673ee2df35792915bd117824b70ef48f
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-14 19:41:50 +0900

    test(ContextV1Test): 전략 패턴 적용 테스트 추가

--- src/test/java/com/example/advanced_spring/trace/strategy/ContextV1Test.java
+++ src/test/java/com/example/advanced_spring/trace/strategy/ContextV1Test.java
@@ -1,5 +1,9 @@
 package com.example.advanced_spring.trace.strategy;
 
+import com.example.advanced_spring.trace.strategy.code.ContextV1;
+import com.example.advanced_spring.trace.strategy.code.strategy.Strategy;
+import com.example.advanced_spring.trace.strategy.code.strategy.StrategyLogic1;
+import com.example.advanced_spring.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -28,5 +32,27 @@ public class ContextV1Test {
 
         long resultTime = endTime - startTime;
         log.info("resultTime = {}", resultTime);
+    }
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+        /**
+         * 19:33:29.600 [Test worker] INFO com.example.advanced_spring.trace.strategy.code.strategy.StrategyLogic1 -- 비즈니스 로직1 실행...
+         * 19:33:29.601 [Test worker] INFO com.example.advanced_spring.trace.strategy.code.ContextV1 -- resultTime: 2
+         */
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+        /**
+         * 19:33:29.602 [Test worker] INFO com.example.advanced_spring.trace.strategy.code.strategy.StrategyLogic2 -- 비즈니스 로직2 실행...
+         * 19:33:29.602 [Test worker] INFO com.example.advanced_spring.trace.strategy.code.ContextV1 -- resultTime: 0
+         */
     }
 }
