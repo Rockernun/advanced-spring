3a74847ad12ccbda186e0977a9e072a2d9a91f4d
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-14 19:02:34 +0900

    feat(AbstractTemplate): 템플릿 메서드 패턴 도입

---
+++ src/main/java/com/example/advanced_spring/trace/template/AbstractTemplate.java
@@ -0,0 +1,31 @@
+package com.example.advanced_spring.trace.template;
+
+import com.example.advanced_spring.trace.TraceStatus;
+import com.example.advanced_spring.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T execute(String message) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin(message);
+
+            T result = call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}
