1a94035a0d72c7156827f755fea9c3b165deabc3
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-16 18:53:14 +0900

    chore(Application): @Import 애노테이션 제거

--- src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
+++ src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV2Config.class)
+//@Import(AppV2Config.class)
 @SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy")
 public class AdvancedSpringApplication {
 
