a79cda76e1e0844f68b36a6b3af2bfba6691f27e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-16 18:44:27 +0900

    feat(ProxyOrderServiceV2): 구체 Service 클래스 추가

---
+++ src/main/java/com/example/advanced_spring/proxy/v2/ProxyOrderServiceV2.java
@@ -0,0 +1,14 @@
+package com.example.advanced_spring.proxy.v2;
+
+public class ProxyOrderServiceV2 {
+
+    private final ProxyOrderRepositoryV2 orderRepository;
+
+    public ProxyOrderServiceV2(ProxyOrderRepositoryV2 orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
