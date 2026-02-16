872635e8aaaf98c045260ec75d66991e1ddf3b8e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-16 18:35:09 +0900

    feat(ProxyOrderControllerV1): Controller 인터페이스와 구현 클래스 추가

---
+++ src/main/java/com/example/advanced_spring/proxy/v1/ProxyOrderControllerV1.java
@@ -0,0 +1,15 @@
+package com.example.advanced_spring.proxy.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface ProxyOrderControllerV1 {
+
+    @GetMapping("/proxy/v1/request")
+    String request(@RequestParam("itemId") String itemId);
+
+    @GetMapping("/proxy/v1/no-log")
+    String noLog();
+}
