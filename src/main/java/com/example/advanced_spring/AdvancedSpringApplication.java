3e27398adebc1d5e6b8cfa158b8667d214290d73
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-16 18:45:08 +0900

    chore(AppV2Config, Application): 설정 정보 적용

--- src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
+++ src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
@@ -1,12 +1,12 @@
 package com.example.advanced_spring;
 
-import com.example.advanced_spring.proxy.config.AppV1Config;
+import com.example.advanced_spring.proxy.v2.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
-@SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy.v3")
+@Import(AppV2Config.class)
+@SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy")
 public class AdvancedSpringApplication {
 
 	public static void main(String[] args) {
