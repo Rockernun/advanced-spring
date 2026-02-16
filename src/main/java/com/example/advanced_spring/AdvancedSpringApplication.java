6aa0cd7305568aa05aca64824b007656e6f55e2d
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-16 18:36:17 +0900

    chore(Application): AppV1Config 설정 정보 적용 및 컴포넌트 스캔 충돌을 피하도록 처리

--- src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
+++ src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
@@ -1,9 +1,12 @@
 package com.example.advanced_spring;
 
+import com.example.advanced_spring.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy.v3")
 public class AdvancedSpringApplication {
 
 	public static void main(String[] args) {
