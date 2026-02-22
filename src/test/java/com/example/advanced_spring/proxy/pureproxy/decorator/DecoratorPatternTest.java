3d553fee00ddf5c16ac7c2d99a10cf8a2f873758
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-22 22:26:08 +0900

    test(DecoratorPatternTest): MessageDecorator로 응답값을 꾸미는 테스트 추가

--- src/test/java/com/example/advanced_spring/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/com/example/advanced_spring/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,8 +2,10 @@ package com.example.advanced_spring.proxy.pureproxy.decorator;
 
 import com.example.advanced_spring.proxy.pureproxy.decorator.code.Component;
 import com.example.advanced_spring.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import com.example.advanced_spring.proxy.pureproxy.decorator.code.MessageDecorator;
 import com.example.advanced_spring.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.message.Message;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -18,6 +20,22 @@ public class DecoratorPatternTest {
         /**
          * 22:06:32.422 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.decorator.code.RealComponent -- RealComponent 실행...
          * 22:06:32.424 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.decorator.code.DecoratorPatternClient -- result=data
+         */
+    }
+
+    @Test
+    void decoratorCase1() {
+        Component realComponent = new RealComponent();
+        MessageDecorator decorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(decorator);
+
+        client.execute();
+
+        /**
+         * 22:19:41.223 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.decorator.code.MessageDecorator -- MessageDecorator 실행...
+         * 22:19:41.225 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.decorator.code.RealComponent -- RealComponent 실행...
+         * 22:19:41.225 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.decorator.code.MessageDecorator -- MessageDecorator 꾸미기 적용 전=data, 적용 후=*****data*****
+         * 22:19:41.226 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.decorator.code.DecoratorPatternClient -- result=*****data*****
          */
     }
 }
