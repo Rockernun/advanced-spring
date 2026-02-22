530abceb02ce6bf8afe373924af403b80bcf0581
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-23 00:03:13 +0900

    feat(Application): 애플리케이션 실행으로 프록시 적용 확인

--- src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
+++ src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
@@ -1,16 +1,24 @@
 package com.example.advanced_spring;
 
-import com.example.advanced_spring.proxy.v2.config.AppV2Config;
+import com.example.advanced_spring.proxy.config.v1_proxy.InterfaceProxyConfig;
+import com.example.advanced_spring.trace.logtrace.LogTrace;
+import com.example.advanced_spring.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-//@Import(AppV2Config.class)
-@SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy")
+@Import(InterfaceProxyConfig.class)
+@SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy.config.v1_proxy")
 public class AdvancedSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedSpringApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
