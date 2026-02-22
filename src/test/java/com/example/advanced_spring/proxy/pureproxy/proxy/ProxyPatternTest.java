794db3f4afe5f178cc73c69fd14f0da9eb05004c
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-22 21:48:37 +0900

    test(ProxyPatternTest): 클라이언트가 프록시 객체를 호출하는 테스트 추가
    - ProxyPatternClient는 Subject 인터페이스에만 의존하고 있기 때문에 프록시를 호출하는지, 실제 객체를 호출하는지 알 수 없다.
    - 최초 한번 execute()를 통해 요청한 이후에는 캐시된 데이터를 가져오기 때문에 값을 즉시 반환받을 수 있다.

--- src/test/java/com/example/advanced_spring/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/com/example/advanced_spring/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,7 +1,9 @@
 package com.example.advanced_spring.proxy.pureproxy.proxy;
 
+import com.example.advanced_spring.proxy.pureproxy.proxy.code.CacheProxy;
 import com.example.advanced_spring.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import com.example.advanced_spring.proxy.pureproxy.proxy.code.RealSubject;
+import com.example.advanced_spring.proxy.pureproxy.proxy.code.Subject;
 import org.junit.jupiter.api.Test;
 
 public class ProxyPatternTest {
@@ -18,6 +20,25 @@ public class ProxyPatternTest {
          * 21:24:00.298 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.proxy.code.RealSubject -- 실제 객체 호출...
          * 21:24:01.301 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.proxy.code.RealSubject -- 실제 객체 호출...
          * 21:24:02.302 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.proxy.code.RealSubject -- 실제 객체 호출...
+         */
+    }
+
+    @Test
+    void cacheProxyTest() {
+        Subject subject = new RealSubject();
+        Subject clientSubject = new CacheProxy(subject);
+        ProxyPatternClient client = new ProxyPatternClient(clientSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+
+        /**
+         * 21:39:55.619 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.proxy.code.CacheProxy -- 프록시 호출...
+         * 21:39:55.621 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.proxy.code.RealSubject -- 실제 객체 호출...
+         *
+         * (기존 호출을 통해 캐시된 값이 존재하므로 값을 즉시 반환)
+         * 21:39:56.623 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.proxy.code.CacheProxy -- 프록시 호출...
+         * 21:39:56.624 [Test worker] INFO com.example.advanced_spring.proxy.pureproxy.proxy.code.CacheProxy -- 프록시 호출...
          */
     }
 }
