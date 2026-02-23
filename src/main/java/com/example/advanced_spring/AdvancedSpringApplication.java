eefa9a402308eeb846e16642b338ed025e3a71e0
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-02-23 14:39:47 +0900

    chore(Application): DynamicProxyBasicConfig 설정 정보 적용

--- src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
+++ src/main/java/com/example/advanced_spring/AdvancedSpringApplication.java
@@ -1,6 +1,7 @@
 package com.example.advanced_spring;
 
 import com.example.advanced_spring.proxy.config.v1_proxy.ConcreteProxyConfig;
+import com.example.advanced_spring.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.example.advanced_spring.trace.logtrace.LogTrace;
 import com.example.advanced_spring.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -8,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(ConcreteProxyConfig.class)
-@SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy.config.v1_proxy")
+@Import(DynamicProxyBasicConfig.class)
+@SpringBootApplication(scanBasePackages = "com.example.advanced_spring.proxy.config.v2_dynamicproxy")
 public class AdvancedSpringApplication {
 
 	public static void main(String[] args) {
