00c9842fc64a563952fafb341e31be0556b69cad
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-03-06 23:40:18 +0900

    test(ProxyFactoryTest): 프록시 팩토리를 통해 생성한 프록시에 대한 테스트 추가
    - 현재 ServiceImpl이 ServiceInterface 인터페이스를 구현하고 있으므로 프록시 팩토리는 JDK 동적 프록시를 기본으로 사용

---
+++ src/test/java/com/example/advanced_spring/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,33 @@
+package com.example.advanced_spring.proxy.proxyfactory;
+
+import com.example.advanced_spring.proxy.common.advice.TimeAdvice;
+import com.example.advanced_spring.proxy.common.service.ServiceImpl;
+import com.example.advanced_spring.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    @Test
+    void interfaceProxy() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        Assertions.assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        Assertions.assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
